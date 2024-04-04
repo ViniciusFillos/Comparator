fb72107bcbbb991401720ed0ffd1e50082794232
Vinicius Fillos <vinifillos@gmail.com>
2024-04-04 20:52:10 -0300

    Anonymous class

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,9 @@
 package application;
 
-import entities.MyComparator;
 import entities.Product;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
@@ -17,8 +16,14 @@ public class App {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparator());
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+            }
+        };
 
+        list.sort(comp);
         System.out.println();
         for(Product p : list) {
             System.out.println(p);
--- src/entities/MyComparator.java
+++ src/entities/MyComparator.java
@@ -1,6 +1,5 @@
 package entities;
 
-
 import java.util.Comparator;
 
 public class MyComparator implements Comparator<Product> {
