92bef9fce5b96791540053a9ca53c9b0f4f63264
Vinicius Fillos <vinifillos@gmail.com>
2024-04-04 22:09:57 -0300

    Lambda 1

--- src/application/App.java
+++ src/application/App.java
@@ -16,11 +16,8 @@ public class App {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = new Comparator<Product>() {
-            @Override
-            public int compare(Product o1, Product o2) {
-                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
-            }
+        Comparator<Product> comp = (p1, p2) -> {
+            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
         };
 
         list.sort(comp);
